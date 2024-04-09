fddd7168b83ca2bb25159f1705a114bfbb6657dc
arya <arya2003p@gmail.com>
2024-04-09 11:09:16 +0530

    final Commit

--- To-Do/src/components/TodoForm.jsx
+++ To-Do/src/components/TodoForm.jsx
@@ -1,18 +1,19 @@
-import React from "react";
-import { useState } from "react";
-import { useTodo } from "../Context";
+import React, { useState } from "react";
+import { useTodo } from "../Context/TodoContext";
 
-const TodoForm = () => {
-    let [todo,setTodo]=useState("")
+function TodoForm() {
+  const [todo, setTodo] = useState("");
+  const { addTodo } = useTodo();
 
-    let{addTodo}=useTodo()
+  const add = (e) => {
+    e.preventDefault();
 
-    let add =(e)=>{
-        e.preventDefault()
-    }
-    if(!todo) return
-    addTodo({todo,completed:false})
-    setTodo("")
+    if (!todo) return;
+
+    addTodo({ todo, completed: false });
+    setTodo("");
+  };
+
   return (
     <form onSubmit={add} className="flex">
       <input
@@ -20,7 +21,7 @@ const TodoForm = () => {
         placeholder="Write Todo..."
         className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
         value={todo}
-        onChange={(e)=>setTodo(e.target.value)}
+        onChange={(e) => setTodo(e.target.value)}
       />
       <button
         type="submit"
@@ -30,6 +31,6 @@ const TodoForm = () => {
       </button>
     </form>
   );
-};
+}
 
 export default TodoForm;
--- To-Do/src/components/TodoItem.jsx
+++ To-Do/src/components/TodoItem.jsx
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import { useTodo } from "../Context/TodoContext";
 
-const TodoItem = () => {
+function TodoItem({ todo }) {
+  const [isTodoEditable, setIsTodoEditable] = useState(false);
+  const [todoMsg, setTodoMsg] = useState(todo.todo);
+  const { updateTodo, deleteTodo, toggleComplete } = useTodo();
+
+  const editTodo = () => {
+    updateTodo(todo.id, { ...todo, todo: todoMsg });
+    setIsTodoEditable(false);
+  };
+  const toggleCompleted = () => {
+    toggleComplete(todo.id);
+  };
+
   return (
     <div
       className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${
@@ -45,6 +58,6 @@ const TodoItem = () => {
       </button>
     </div>
   );
-};
+}
 
 export default TodoItem;
